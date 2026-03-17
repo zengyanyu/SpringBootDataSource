dec36369749f873427ff69b2d8ad9e3fc82145e6
zengyanyu <1194314874@qq.com>
2026-03-17 11:49:13 +0800

    添加

--- src/main/java/com/zengyanyu/config/SwaggerConfig.java
+++ src/main/java/com/zengyanyu/config/SwaggerConfig.java
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2026, 曾衍育 All rights reserved.
- * 自定义License声明
- * ZENGYANYU PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
 package com.zengyanyu.config;
 
 import org.springframework.context.annotation.Bean;
--- src/main/java/com/zengyanyu/query/QueryObject.java
+++ src/main/java/com/zengyanyu/query/QueryObject.java
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2026, 曾衍育 All rights reserved.
- * 自定义License声明
- * ZENGYANYU PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
 package com.zengyanyu.query;
 
 import io.swagger.annotations.ApiModel;
